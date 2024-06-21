12ab22ce1ad90313e02964a0940050dc1f8d00a8
Daniela Soares <danielasmelo232@gmail.com>
2024-06-21 20:00:49 -0300

    linguagem

---
+++ ExerciciosPraticos/ExercicioPratico01/ExercicioPraticoII/Program.cs
@@ -0,0 +1,7 @@
+﻿//aula 70
+Console.WriteLine("Exercício Prático II\n");
+Console.WriteLine("g- Crie um construtor que permita criar um carro somente usando os atributos : Modelo e Montadora (não use a palavra-chave this)" +
+    "\ni- Crie um método chamado VelocidadeMaxima que recebe um parâmetro do tipo int chamado potencia que vai retornar um double representando a velocidade máxima do carro" +
+    "( Para calcular a velocidade multiplique a potencia por 1.75)" +
+    "\r\nh- Explique qual o comportamento ao usar este construtor para criar objetos do tipo Carro" +
+    "\r\nj- Explique o comportamento da palavra return");
